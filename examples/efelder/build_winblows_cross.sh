75be6f60576060e8ad2c5a1234eb06422e565614
Captain4LK <captain4lk@tutanota.com>
2020-11-12 18:34:43 +0100

    Update windows_cross scripts

--- examples/efelder/build_winblows_cross.sh
+++ examples/efelder/build_winblows_cross.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd "$(dirname "$0")"
-i686-w64-mingw32-gcc -O3 -o efelder *.c ../../external/UtilityLK/src/ULK_vector.c -L../../lib/winblows_cross -lSLK -lmingw32 -lSDL2main -lSDL2 -lopengl32 -mwindows -Wall
+i686-w64-mingw32-gcc -O3 -o efelder *.c ../../external/UtilityLK/src/ULK_vector.c -L../../lib/windows_cross -lSLK -lmingw32 -lSDL2main -lSDL2 -lopengl32 -mwindows -Wall
 exit
--- examples/performance/build_winblows_cross.sh
+++ examples/performance/build_winblows_cross.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd "$(dirname "$0")"
-i686-w64-mingw32-gcc -O3 -o performance main.c -L../../lib/winblows_cross -lSLK -lmingw32 -lSDL2main -lSDL2  -lopengl32 -mwindows -Wall
+i686-w64-mingw32-gcc -O3 -o performance main.c -L../../lib/windows_cross -lSLK -lmingw32 -lSDL2main -lSDL2  -lopengl32 -mwindows -Wall
 exit
 
